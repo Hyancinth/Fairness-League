278bce7c2edfa62603015f7864885ecc454ce751
Ethan Tran <ethan.benj.tran@gmail.com>
2026-03-16 14:43:22 -0400

    Switch from gesture regonition to hand larkmark detection

--- app/camera.py
+++ app/camera.py
@@ -1,31 +1,28 @@
 import cv2
 from pathlib import Path
+import numpy as np
+
 import time
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-MODEL_PATH = Path(__file__).resolve().parent / 'models' / 'gesture_recognizer.task'
-GESTURE_MAP = {
-    "Closed_Fist": "Rock",
-    "Open_Palm": "Paper",
-    "Victory": "Scissors"
-}
+MODEL_PATH = Path(__file__).resolve().parent / 'models' / 'hand_landmarker.task'
 
 class VideoCamera():
     def __init__(self):
         self.cap = cv2.VideoCapture(0) 
-        self.start_time = time.time()
+        self.timestamp = 0
 
-        # load model
+        # load and setup model
         base_options = python.BaseOptions(model_asset_path=str(MODEL_PATH))
-        options = vision.GestureRecognizerOptions(base_options=base_options,
+        options = vision.HandLandmarkerOptions(base_options=base_options,
             running_mode=vision.RunningMode.LIVE_STREAM,
-            result_callback=self.recognize_callback
+            result_callback=self.landmark_callback
             )
-        self.recognizer = vision.GestureRecognizer.create_from_options(options)
+        self.landmarker = vision.HandLandmarker.create_from_options(options)
 
-        self.latest_gesture = None
+        self.result = ""
     
     def get_frame(self):
         success, frame = self.cap.read()
@@ -38,21 +35,45 @@ class VideoCamera():
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data=rgb_frame)
 
-        timestamp_mps = int((time.time() - self.start_time) * 1000)
-        self.recognizer.recognize_async(mp_image, timestamp_mps)
+        self.timestamp += 1
+        self.landmarker.detect_async(mp_image, self.timestamp)
         
-        if self.latest_gesture:
-            cv2.putText(frame, self.latest_gesture, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        else:
-            cv2.putText(frame, "No gesture detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if self.result:
+            cv2.putText(frame, self.result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # else:
+        #     cv2.putText(frame, "No gesture detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         _, jpeg = cv2.imencode('.jpg', frame)
 
         return jpeg.tobytes()
 
-    def recognize_callback(self, result, output_image, timestamp_ms):
+    def landmark_callback(self, result, output_image, timestamp_ms):
         if result:
-            gesture = result.gestures[0][0]
-            self.latest_gesture = f"{GESTURE_MAP.get(gesture.category_name, gesture.category_name)} ({gesture.score:.2f})"
+            try:
+                landmarks = result.hand_landmarks[0]
+            except IndexError:
+                self.result = ""
+                return
+
+            def finger_extended(tip, pip, wrist):
+                tip_to_wrist = np.linalg.norm(np.array([tip.x - wrist.x, tip.y - wrist.y, tip.z - wrist.z]))
+                pip_to_wrist = np.linalg.norm(np.array([pip.x - wrist.x, pip.y - wrist.y, pip.z - wrist.z]))
+
+                return tip_to_wrist > pip_to_wrist
+            
+            index = finger_extended(landmarks[8], landmarks[6], landmarks[0])
+            middle = finger_extended(landmarks[12], landmarks[10], landmarks[0])
+            ring = finger_extended(landmarks[16], landmarks[14], landmarks[0])
+            pinky = finger_extended(landmarks[20], landmarks[18], landmarks[0])
+
+            if not index and not middle and not ring and not pinky:
+                self.result = "Rock"
+            elif index and middle and not ring and not pinky:
+                self.result = "Scissors"
+            elif index and middle and ring and pinky:
+                self.result = "Paper"
+            else:
+                self.result = "Unknown"
+
         else:
-            self.latest_gesture = ""
+            self.result = ""
